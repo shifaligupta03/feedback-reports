b17cfb4acdcdc77fc535105c1dd5b1d5e6474718
shifali <shifali.gupta183@gmail.com>
2019-07-07 12:23:03 +0530

    authentication by passport

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport');
 const app = express();
-const keys = require('./config/keys');
 
+require('./routes/authroutes')(app);
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret:keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken)=>{
-    console.log(accessToken);
-}));
-
-app.get('/auth/google', passport.authenticate('google',{
-    scope:['profile','email']
-}))
 
 const PORT = process.env.PORT ||5000;
 app.listen(PORT);
