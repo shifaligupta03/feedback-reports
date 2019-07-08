ed94b75db93ed4d95d18baa85618b58711fe1bc6
shifali <shifali.gupta183@gmail.com>
2019-07-09 00:09:26 +0530

    model user

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
+require('./models/user');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
 
 const app = express();
 
