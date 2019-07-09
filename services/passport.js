e9f1563906419b4957606aab0946af00ad27b61c
shifali <shifali.gupta183@gmail.com>
2019-07-10 00:12:08 +0530

    passport

--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,23 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret:keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'http://localhost:5000/auth/google/callback'
 }, (accessToken, refreshToken, profile, done)=>{
-    console.log({accessToken, refreshToken, profile, done});
+    console.log(profile.id);
+   User.findOne({googleId: profile.id})
+    .then((existingUser)=>{
+        if(existingUser){
+            done(null, user);
+        } else {
+            new User({ googleId: profile.id }).save()
+            .then(user=>done(null, user));
+        }
+    });
+    
 }));
