0d409cc91fa795dd470ae34820e868245d9c7825
shifali <shifali.gupta183@gmail.com>
2019-07-11 00:02:11 +0530

    passport

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id)
+    .then(user=>{
+        done(null, user);
+    })
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret:keys.googleClientSecret,
