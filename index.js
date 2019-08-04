3abe94fc7c4c9e4e36de908ee61b443fd3148d2a
shifali <shifali.gupta183@gmail.com>
2019-08-04 14:12:42 +0530

    passport authentication

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
-
-
-
 mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys:[keys.cookieKey]
+    })
+);
+
+// tell passport to use cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authroutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -24,9 +24,10 @@ passport.use(new GoogleStrategy({
    User.findOne({googleId: profile.id})
     .then((existingUser)=>{
         if(existingUser){
-            done(null, user);
+            done(null, existingUser);
         } else {
-            new User({ googleId: profile.id }).save()
+            new User({ googleId: profile.id })
+            .save()
             .then(user=>done(null, user));
         }
     });
