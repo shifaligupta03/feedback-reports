70653da6b29eea01cfc1d8dea7bd50cfbeea2650
shifali <shifali.gupta183@gmail.com>
2019-08-14 00:53:18 +0530

    actions and reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-export const fetchUser = ()=>{
-    return function(dispatch){
-        axios.get('/api/checkUser')
-        .then(res=>dispatch({type: FETCH_USER, payload:res}));
-    }
-}
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/checkUser');
+  dispatch({type: FETCH_USER, payload: res.data});
+};
