254c1cb4215d4b4f6fd4292d373c82036dfbe970
shifali <shifali.gupta183@gmail.com>
2019-08-21 10:16:35 +0530

    Header and Linking done

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,14 +3,14 @@ module.exports = function(app) {
   app.use(
     proxy('/auth/google', {
       target: 'http://localhost:5000/',
-      changeOrigin: true,
+      // changeOrigin: true,
     })
   );
 
   app.use(
     proxy('/api/*', {
       target: 'http://localhost:5000/',
-      changeOrigin: true,
+      // changeOrigin: true,
     })
   );
 }
