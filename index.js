ba7d847cda732eb98a58bc08081b3db1e87846bc
shifali <shifali.gupta183@gmail.com>
2019-08-23 11:07:33 +0530

    handling payments in stripe frontend and backend

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport');
 mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30*24*60*60*1000,
@@ -21,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authroutes')(app);
+require('./routes/billingroutes')(app);
 
 
 const PORT = process.env.PORT ||5000;
