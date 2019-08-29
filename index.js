69bbf0852c3101b2a6dfed853d743e6acea0904f
shifali <shifali.gupta183@gmail.com>
2019-08-29 09:43:48 +0530

    surveys models

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-require('./models/user');
+require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 mongoose.connect(keys.mongoURI,{ useNewUrlParser: true });
 
