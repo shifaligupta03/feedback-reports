061f0c91caa3b40177349fd4a6f5d4e371ba1070
shifali <shifali.gupta183@gmail.com>
2019-09-16 09:36:51 +0530

    email template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -4,6 +4,13 @@ module.exports =(survey)=>{
         <body>
             <div style="text-align:center;">
             <p>Please answer the following questions</p>
+            <p>${survey.body}</p>
+            <div>
+                <a href="">Yes</a>
+            </div>
+            <div>
+                <a href="">No</a>
+            </div>
         </body
        </html> 
     `
