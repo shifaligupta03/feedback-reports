acf7744046a0ec3a1912d58c08c0aabcc2518da8
shifali <shifali.gupta183@gmail.com>
2019-09-17 09:30:47 +0530

    polishing route handlers

--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('Surveys');
 
 module.exports = app=>{
-    app.post('/api/surveys', requireLogin, requireCredits, (req,res)=>{
+    app.post('/api/surveys', requireLogin, requireCredits, async (req,res)=>{
         const { title, subject, body, recipients} = req.body;
 
         const survey = new Survey({
@@ -19,7 +19,17 @@ module.exports = app=>{
             dateSent: Date.now()
         });
 
-        const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
+        try{
+            const mailer = new Mailer(survey, surveyTemplate(survey));
+            await mailer.send(); 
+            await survey.save();
+            req.user.credits -=1;
+            const user =await req.user.save();
+            req.send(user);
+        } catch(err){
+            req.status(422).send(err);
+
+        }
+        
     });
 }
