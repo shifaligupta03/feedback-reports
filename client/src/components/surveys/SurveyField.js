28358e3527ca51185fc64f017fe9246cd5fae7a7
shifali <shifali.gupta183@gmail.com>
2019-09-30 09:56:12 +0530

    fields redux form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 
-export default ({input}) => {
+export default ({input, label}) => {
 
 
     return (
         <div>
+            <label>{label}</label>
             <input {...input} />
         </div>
     )
