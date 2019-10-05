9caf96121022192a5da12ea1083d20e86e98cb50
shifali <shifali.gupta183@gmail.com>
2019-10-05 16:08:57 +0530

    form review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class SurveyReview extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <h5>Please confirm your entries</h5>
-            </div>
-        
-         );
-    }
-}
- 
+const SurveyReview = () => {
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+    </div>
+  );
+};
+
 export default SurveyReview;
