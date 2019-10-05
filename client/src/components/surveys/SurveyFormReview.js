5b60a7d24326787c96ad981d00624f4c8aeae9df
shifali <shifali.gupta183@gmail.com>
2019-10-05 16:01:01 +0530

    form and form review

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SurveyReview extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h5>Please confirm your entries</h5>
+            </div>
+        
+         );
+    }
+}
+ 
+export default SurveyReview;
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-    render() {
-        return (<div>
-            <SurveyForm />
-        </div>);
+  state = {showFormReview: false};
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
     }
+
+    return <SurveyForm onSurveySubmit={()=> this.setState({showFormReview: true})} />;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
 
 export default SurveyNew;
